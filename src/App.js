0f04d13a6a8974beb3e97d9bba15872e8325b0d9
Kate <clahfclns@gmail.com>
2022-12-23 23:51:11 +0200

    Add meanings

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Dictionary from './Dictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <main>
+          <Dictionary/>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
