d693ccd7d7417eb9e2ac8a512c8d2f7bc0d2a4e7
Kate <clahfclns@gmail.com>
2023-01-06 00:47:08 +0200

    Add Photos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <main>
           <Dictionary/>
+          <footer>
+          <a href="https://github.com/fwhoami/dictionary-project">
+            Open source code</a>{" "}
+            by fwhoami
+           </footer>
         </main>
       </header>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("Sunset");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+
   }
 
   function search() {
         // documentation: https://dictionaryapi.dev/e
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionResponse);
+
+        const pexelsApiKey = "563492ad6f917000010000010c62c2355ab441a5a19ec161ec4096a6";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;    
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+        axios
+        .get(pexelsApiUrl, {headers: headers }).then(handlePexelsResponse);    
   }
 
   function handleSubmit(event) {
@@ -44,6 +59,7 @@ export default function Dictionary() {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
